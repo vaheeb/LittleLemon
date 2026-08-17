a5a8c870af130469579a4c11db080e508e128805
Vaheeb <kmvaheeb@gmail.com>
2026-08-17 18:24:19 +0530

    Configure MySQL database

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
