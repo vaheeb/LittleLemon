01c7f9927d2a8a5e323a0564436b618a14df5d63
Vaheeb <kmvaheeb@gmail.com>
2026-08-17 18:52:57 +0530

    Implement Menu API

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Menu
+from .serializers import MenuSerializer
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
