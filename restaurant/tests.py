3b3f08fc2ba50e59701a60ba584ebac8bfa2eb66
Vaheeb <kmvaheeb@gmail.com>
2026-08-18 11:28:42 +0530

    Add API unit tests

--- restaurant/tests.py
+++ restaurant/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from .models import Menu
+
+
+class MenuAPITestCase(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.menu = Menu.objects.create(
+            Title="Biryani",
+            Price=120.00,
+            Inventory=5
+        )
+
+    def test_get_menu_items(self):
+        response = self.client.get("/restaurant/menu/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 1)
+
+    def test_create_menu_item(self):
+        response = self.client.post(
+            "/restaurant/menu/",
+            {
+                "Title": "Pasta",
+                "Price": "150.00",
+                "Inventory": 10
+            },
+            format="json"
+        )
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Menu.objects.count(), 2)
